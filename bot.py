cbe38b479a22d83ea2bc0c954e2967a2158cb3e2
PaulieGlot <paulvg01@gmail.com>
2024-04-05 14:07:08 -0500

    testing

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ def get_section_name(book: str):
     section = ""
     for line in file:
         if line.startswith(b'#'):
-            section = line.removeprefix("#")
+            section = line.removeprefix(b'#')
         if line.startswith("%s," % book):
             return section.removesuffix("\n")
     return "error finding section name: book `%s` is not listed in chapters.txt.\n\n*jan Poli says: make sure you're using the same book names as this version (these are case-sensitive), and that you have all planned books listed along with their chapter lengths!*" % book
