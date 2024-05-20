faf600d611ff8008c806aa255602e8ce75b0e7b7
PaulieGlot <116028434+PaulieGlot@users.noreply.github.com>
2024-05-20 02:14:46 -0500

    Update bot.py

--- code/bot.py
+++ code/bot.py
@@ -176,7 +176,7 @@ async def cite(ctx, citation: str, euphemise: bool=True, post: bool=False):
 @tree.command(name="help", description="stop it. get some help", guild=discord.Object(id=GUILD_ID))
 async def help(ctx, command: str=None, post: bool=False):
     if command is None:
-        await respond(ctx, "/help - display this help text\n/verse - fetch a specified verse\n/range - fetch a specified range of verses\ncite - use a biblical citation\n/repo - get a link to the repo", post)
+        await respond(ctx, "/help - display this help text\n/verse - fetch a specified verse\n/range - fetch a specified range of verses\ncite - use a biblical citation\n/repo - get a link to the repo\n/stats - get some quick stats", post)
     elif command == "help":
         await respond(ctx, "what... what more do you need", post)
     elif command == "verse":
@@ -187,6 +187,8 @@ async def help(ctx, command: str=None, post: bool=False):
         await respond(ctx, "specify a verse or range using the traditional biblical citation format. make sure you're using the same book names as this version!", post)
     elif command == "repo":
         await respond(ctx, "get a link to the repo from which this bot is pulling verses", post)
+    elif command == "stats":
+        await respond(ctx, "get the recorded stats from the last time buildbook was run", post)
 
 
 @tree.command(name="repo", description="get a link to the repo from which this bot is pulling verses", guild=discord.Object(id=GUILD_ID))
