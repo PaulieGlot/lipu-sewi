fac10fb2cbff4562331e5bf83c6085e9e98d2e3e
PaulieGlot <paulvg01@gmail.com>
2025-04-25 01:47:16 -0500

    bot fixes

--- code/bot2.py
+++ code/bot2.py
@@ -106,8 +106,8 @@ class Bot:
 
         self.intents = discord.Intents.default()
         self.intents.messages = True
-        self.client = discord.Client(intents=intents)
-        self.tree = app_commands.CommandTree(client)
+        self.client = discord.Client(intents=self.intents)
+        self.tree = app_commands.CommandTree(self.client)
         self.engine = Engine(repo)
         self.client.run(self.TOKEN)
 
